c28c3491bb328dbd1b0b13eb59d5ca23984bbed3
maimai <mailys.andrieu.simplon@gmail.com>
2024-10-02 15:42:49 +0200

    creation table order

--- crmtp/src/main/java/com/crmtp/crmtp/Entities/Order.java
+++ crmtp/src/main/java/com/crmtp/crmtp/Entities/Order.java
@@ -1,4 +1,42 @@
 package com.crmtp.crmtp.Entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.query.sqm.tree.expression.NumericTypeCategory;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "orders")
+@Data
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(name="service_type",length=100 ,nullable = false)
+    private String service_type;
+
+    @Column(name="tva",columnDefinition = "Numeric",nullable = false)
+    private BigDecimal tva;
+
+    @Column(name="nb_days",nullable = false)
+    private int nb_day;
+
+    @Column(name="total_exclude_tax", columnDefinition = "Numeric", nullable = false)
+    private BigDecimal total_exclude_tax;
+
+    @Column(name="state")
+    private int state;
+
+    @Column(name="comment", columnDefinition = "TEXT")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name="client_id", referencedColumnName = "id")
+    private Customer customer;
+
+
+
 }
